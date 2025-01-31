3169ecebdd55fc136e0fee66e19fe247e4060cf4
l1f22bscs0122 <145602391+nauman632@users.noreply.github.com>
2025-02-01 03:55:51 +0500

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.1")
+        classpath("com.google.gms:google-services:4.4.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24") // Add this line
+    }
+}
